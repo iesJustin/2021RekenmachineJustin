c01f63550b028d398414880eaccb63d7bb6ebfec
iesJustin <justinpa43@gmail.com>
2023-04-20 13:02:47 +0200

    Added Plus feature

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -3,7 +3,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
     @Test
-    void aDummyTest() {
-        assertEquals(0,0);
+    void testAdd() {
+        //arrange
+        JavaFXApp javaFXApp = new JavaFXApp();
+        int number1 = 2;
+        int number2 = 3;
+        //act
+        int verwacht = 5;
+        int actual = javaFXApp.computeAdd(number1,number2);
+        //assert
+        assertEquals(verwacht,actual);
     }
 }
